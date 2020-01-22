b58cb64676a20eab43413daaafe38bf2dabc67a6
Gabriel Gomes <bibiel2605@gmail.com>
2020-01-22 19:08:40 -0300

    React My Money APP

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2020
+            <a href='https://www.linkedin.com/in/gabriel-gomes-da-cunha-ab155b185/' target='_blank'> Gabriel Gomes</a>.
+        </strong>
+    </footer>
+)
